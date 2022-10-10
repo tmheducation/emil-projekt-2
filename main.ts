f480565aa98a478eb9ec3672dd55a8d82517eb61
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-10 11:51:27 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.setLedColor(0xff0000)
-    music.playMelody("G F G A - F E D ", 120)
+    music.playMelody("G F G A - F E D ", 85)
+    basic.setLedColor(0xff0000)
+    basic.pause(500)
+    basic.setLedColor(0x00ffff)
+    basic.pause(500)
+    basic.setLedColor(0x007fff)
+    basic.pause(500)
+    basic.setLedColor(0x00ff00)
+    basic.pause(500)
+    basic.setLedColor(0x000000)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.showLeds(`
